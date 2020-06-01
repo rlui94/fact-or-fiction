17cfb5ab30ffc41bb973407d00c8b86e185932af
Ryan Lui <ryanlui.college@gmail.com>
2020-06-01 16:32:47 -0700

    tutorial practice

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Employees</h1>
+    <employee-table :employees="employees" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import EmployeeTable from '@/components/EmployeeTable.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    EmployeeTable,
+  },
+  data() {
+    return {
+      employees: [
+        {
+          id: 1,
+          name: 'Dick Hendricks',
+          email: 'richard@piedpiper.com',
+        },
+        {
+          id: 2,
+          name: 'Bertram Gilfoyle',
+          email: 'gilfoyle@piedpiper.com',
+        },
+        {
+          id: 3,
+          name: 'Dinesh Chugtai',
+          email: 'dinesh@piedpiper.com',
+        },
+      ],
+    }
+  },
 }
 </script>
 
