03b8e6a5011a5837e40597756c8787f1a510987f
Ryan Lui <ryanlui.college@gmail.com>
2020-06-02 20:10:32 -0700

    add url meta scraper for image src

---
+++ src/api/UrlMeta.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+const api_url = 'https://api.urlmeta.org/';
+const proxyurl="https://cors-anywhere.herokuapp.com/"
+
+export default{
+    getUrlMeta(address){
+        let b64Cred = btoa(process.env.VUE_APP_URLMETA_EMAIL + ':' + process.env.VUE_APP_URLMETA_KEY)
+        return axios.get(
+            proxyurl + api_url + "?url=" + address,
+            {
+                headers:{
+                    'Authorization': 'Basic ' + b64Cred,
+                }
+            }
+        )
+        .then(response=>{
+            console.log('URL Meta request successful', response);
+            return response.data.meta;
+        })
+        .catch(error =>{
+            console.log('Request failed', error);
+        })
+    }
+}
