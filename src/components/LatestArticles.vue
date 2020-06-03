a8ee6b975538d2aed25565af89a760890345011d
Ryan Lui <ryanlui.college@gmail.com>
2020-06-02 20:56:31 -0700

    modify to use new image component

--- src/components/LatestArticles.vue
+++ src/components/LatestArticles.vue
@@ -2,9 +2,11 @@
   <div id="latest-articles">
     <ul v-for="art in articles" :key="art.id">
         <li>
-            <h5>{{ art.title }}</h5>
+            <h5><a :href="art.canonical_url">{{ art.title }}</a></h5>
+            <image-component :url="art.canonical_url"/>
             <h6>Domain: {{ art.domain }}</h6>
-            <p>Site Tags: <span v-for="tag in art.site_tags" :key="tag.name">{{ tag.name }} </span></p>
+            <p v-if="art.site_tags === null">Site Tags: None</p>
+            <p v-else>Site Tags: <span v-for="tag in art.site_tags" :key="tag.name">{{ tag.name }} </span></p>
         </li>
     </ul>
   </div>
@@ -12,21 +14,30 @@
 
 <script>
 import HoaxyAPI from '@/api/Hoaxy'
+import ImageComponent from '@/components/Image'
+
+const maxArticles = 2;
+
 export default {
   name: 'latest-articles',
+  components: {
+      ImageComponent,
+  },
 
   data(){
     return{
-      articles: [],
+        articles: [],
     }
   },
 
   created(){
     HoaxyAPI.getLatestArticles(2)
-      .then(articles =>{
-        this.articles = articles
-      })
-  }
+      .then(result =>{
+        this.articles = result.slice(0, maxArticles);
+      });
+  },
+
+  methods:{},
 }
 </script>
 
