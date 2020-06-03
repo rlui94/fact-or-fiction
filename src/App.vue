22fc917bcee2b3fca545241854296e1972331ac8
Ryan Lui <ryanlui.college@gmail.com>
2020-06-02 20:08:22 -0700

    clean up app.vue

--- src/App.vue
+++ src/App.vue
@@ -2,41 +2,20 @@
   <div id="app">
     <h1>Latest Articles</h1>
     <latest-articles />
-    <h1>Employees</h1>
-    <employee-table :employees="employees" />
     
   </div>
 </template>
 
 <script>
-import EmployeeTable from '@/components/EmployeeTable.vue'
 import LatestArticles from '@/components/LatestArticles.vue'
 
 export default {
   name: 'App',
   components: {
-    EmployeeTable,
     LatestArticles,
   },
   data() {
     return {
-      employees: [
-        {
-          id: 1,
-          name: 'Dick Hendricks',
-          email: 'richard@piedpiper.com',
-        },
-        {
-          id: 2,
-          name: 'Bertram Gilfoyle',
-          email: 'gilfoyle@piedpiper.com',
-        },
-        {
-          id: 3,
-          name: 'Dinesh Chugtai',
-          email: 'dinesh@piedpiper.com',
-        },
-      ],
     }
   },
 }
