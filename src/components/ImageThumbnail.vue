56d8982f99d2c3457b589e16a6541153594ac1a9
Ryan Lui <ryanlui.college@gmail.com>
2020-06-03 21:45:49 -0700

    allow disabling of api call to save on monthly quota

--- src/components/ImageThumbnail.vue
+++ src/components/ImageThumbnail.vue
@@ -8,6 +8,7 @@
 <script>
 import UrlMeta from '@/api/UrlMeta'
 const img_not_found = 'https://www.publicdomainpictures.net/pictures/280000/velka/not-found-image-15383864787lu.jpg'
+const no_api_call = true;
 
 export default {
   name: 'image-thumbnail',
@@ -27,9 +28,14 @@ export default {
 
   methods:{
       async getLink(){
-          let data = await UrlMeta.getUrlMeta(this.url);
-          if(data.result.status === "OK" && typeof data.meta.image != "undefined"){
-              this.link = data.meta.image;
+          if(no_api_call){
+            this.link = img_not_found;
+          }
+          else{
+            let data = await UrlMeta.getUrlMeta(this.url);
+            if(data.result.status === "OK" && typeof data.meta.image != "undefined"){
+                this.link = data.meta.image;
+            }
           }
       }
   }
