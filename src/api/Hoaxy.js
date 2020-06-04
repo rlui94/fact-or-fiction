5840d87bba2b99a615b664ba4a3192e68fff7dcb
Ryan Lui <ryanlui.college@gmail.com>
2020-06-03 23:41:07 -0700

    initial search implement

--- src/api/Hoaxy.js
+++ src/api/Hoaxy.js
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios from 'axios';
 const url = 'https://api-hoaxy.p.rapidapi.com/';
 const x_rapidapi_host = "api-hoaxy.p.rapidapi.com";
 const proxyurl="https://cors-anywhere.herokuapp.com/";
@@ -11,7 +11,7 @@ export default {
     */
     getLatestArticles(past_hours){
         return axios.get(
-            proxyurl + url +  "latest-articles?past_hours=" + past_hours,
+            proxyurl + url + "latest-articles?past_hours=" + past_hours,
             {
                 headers: {
                     "x-rapidapi-host": x_rapidapi_host,
@@ -30,7 +30,7 @@ export default {
 
     getTopArticles(){
         return axios.get(
-            proxyurl + url +  "top-articles",
+            proxyurl + url + "top-articles",
             {
                 headers: {
                     "x-rapidapi-host": x_rapidapi_host,
@@ -42,6 +42,31 @@ export default {
             })
             .then(response=>{
                 console.log('Hoaxy top request successful', response);
+                console.log('Data', response.data);
+                return response.data.articles;
+            })
+            .catch(error =>{
+                console.log('Request failed', error);
+            })
+    },
+
+    searchArticles(query_str){
+        return axios.get(
+            proxyurl + url + "articles",
+            {
+                headers: {
+                    "x-rapidapi-host": x_rapidapi_host,
+                    "x-rapidapi-key": process.env.VUE_APP_RAPIDAPI_KEY,
+                    "useQueryString": true,
+                },
+                query:{
+                    "sort_by":"relevant",
+                    "use_lucene_syntax":"true",
+                    "query": query_str,
+                }
+            })
+            .then(response=>{
+                console.log('Hoaxy article request successful', response);
                 console.log('Data', response.data);
                 return response.data.articles;
             })
---
+++ src/components/SearchArticles.vue
@@ -0,0 +1,42 @@
+<template>
+    <b-container>
+        <b-row><b-col><h2>Article Search</h2></b-col></b-row>
+        <b-row><b-col><h4>Heard about something weird? Research it here!</h4></b-col></b-row>
+        <b-row><b-col>
+            <b-form>
+                <label class="sr-only" for="search-input">Search Terms:</label>
+                <b-form-group 
+                    id="search-input-group"
+                    
+                >
+                    <b-form-input
+                        id="search-input"
+                        v-model="form.query"
+                        required
+                        placeholder="pizzagate AND Clinton"
+                    >
+                    </b-form-input>
+                </b-form-group>
+                <b-button type="submit" variant="primary">Submit</b-button>
+            </b-form>
+        </b-col></b-row>
+    </b-container>
+</template>
+
+<script>
+export default {
+    name: "search-articles",
+
+    data() {
+      return {
+        form: {
+          query:"",
+        },
+      }
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
