580285bacb4435b3bea6aa1c988652910b8ba447
Ryan Lui <ryanlui.college@gmail.com>
2020-06-03 22:04:32 -0700

    made more responsive

--- src/components/LatestArticles.vue
+++ src/components/LatestArticles.vue
@@ -1,26 +1,27 @@
 <template>
   <b-container id="latest-articles">
-      <b-row>
-        <b-col v-for="art in articles" :key="art.id">
-            <b-card no-body
-                style="max-width: 20rem;"
-                class="mb-2 b-col"
-            >    
-                <b-card-body>
-                    <b-link :to="art.canonical_url">
-                        <h4>{{art.title}}</h4>
-                        <image-thumbnail :url="art.canonical_url"/>
-                    </b-link>
-                </b-card-body>
-                
-                <b-card-text>
-                    <h6>Domain: {{ art.domain }}</h6>
-                    <p v-if="art.site_tags === null">Site Tags: None</p>
-                    <p v-else>Site Tags: <span v-for="tag in art.site_tags" :key="tag.name">{{ tag.name }} </span></p>
-                </b-card-text>
-            </b-card>
-        </b-col>
-      </b-row>
+    <b-row><h1>Latest Articles of the Past {{ hours }} Hours</h1></b-row>
+    <b-row>
+      <b-col sm="auto" v-for="art in articles" :key="art.id">
+          <b-card no-body
+              style="max-width: 20rem;"
+              class="mb-2"
+          >    
+              <b-card-body>
+                  <b-link :to="art.canonical_url">
+                      <h4>{{art.title}}</h4>
+                      <image-thumbnail :url="art.canonical_url"/>
+                  </b-link>
+              </b-card-body>
+              
+              <b-card-text>
+                  <h6>Domain: {{ art.domain }}</h6>
+                  <p v-if="art.site_tags === null">Site Tags: None</p>
+                  <p v-else>Site Tags: <span v-for="tag in art.site_tags" :key="tag.name">{{ tag.name }} </span></p>
+              </b-card-text>
+          </b-card>
+      </b-col>
+    </b-row>
   </b-container>
 </template>
 
@@ -28,7 +29,8 @@
 import HoaxyAPI from '@/api/Hoaxy'
 import ImageThumbnail from '@/components/ImageThumbnail'
 
-const maxArticles = 0;
+const maxArticles = 3;
+const hours = 3;
 
 export default {
   name: 'latest-articles',
@@ -37,8 +39,10 @@ export default {
   },
 
   data(){
+    
     return{
-        articles: [],
+      articles: [],
+      hours: hours,
     }
   },
 
--- src/components/TopArticles.vue
+++ src/components/TopArticles.vue
@@ -1,26 +1,27 @@
 <template>
   <b-container id="top-articles">
-      <b-row>
-        <b-col v-for="art in articles" :key="art.id">
-            <b-card no-body
-                style="max-width: 20rem;"
-                class="mb-2 b-col"
-            >    
-                <b-card-body>
-                    <b-link :to="art.canonical_url">
-                        <h4>{{art.title}}</h4>
-                        <image-thumbnail :url="art.canonical_url"/>
-                    </b-link>
-                </b-card-body>
-                
-                <b-card-text>
-                    <h6>Domain: {{ art.domain }}</h6>
-                    <p v-if="art.site_tags === null">Site Tags: None</p>
-                    <p v-else>Site Tags: <span v-for="tag in art.site_tags" :key="tag.name">{{ tag.name }} </span></p>
-                </b-card-text>
-            </b-card>
-        </b-col>
-      </b-row>
+    <b-row><h1>Top Articles of the Past Month</h1></b-row>
+    <b-row>
+      <b-col sm="auto" v-for="art in articles" :key="art.id">
+          <b-card no-body
+              style="max-width: 20rem;"
+              class="mb-2 b-col"
+          >    
+              <b-card-body>
+                  <b-link :to="art.canonical_url">
+                      <h4>{{art.title}}</h4>
+                      <image-thumbnail :url="art.canonical_url"/>
+                  </b-link>
+              </b-card-body>
+              
+              <b-card-text>
+                  <h6>Domain: {{ art.domain }}</h6>
+                  <p v-if="art.site_tags === null">Site Tags: None</p>
+                  <p v-else>Site Tags: <span v-for="tag in art.site_tags" :key="tag.name">{{ tag.name }} </span></p>
+              </b-card-text>
+          </b-card>
+      </b-col>
+    </b-row>
   </b-container>
 </template>
 
@@ -28,7 +29,7 @@
 import HoaxyAPI from '@/api/Hoaxy'
 import ImageThumbnail from '@/components/ImageThumbnail'
 
-const maxArticles = 0;
+const maxArticles = 3;
 
 export default {
   name: 'top-articles',
@@ -43,7 +44,7 @@ export default {
   },
 
   created(){
-    HoaxyAPI.getLatestArticles(2)
+    HoaxyAPI.getTopArticles()
       .then(result =>{
         this.articles = result.slice(0, maxArticles);
       });
